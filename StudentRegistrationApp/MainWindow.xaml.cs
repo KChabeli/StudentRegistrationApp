bb61398d37dc08a425bf9f9f19acb0a8529848ea
KChabeli <166944138+KChabeli@users.noreply.github.com>
2025-06-21 19:45:52 +0200

    SRA

---
+++ StudentRegistrationApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using StudentRegistrationApp.Models;
+using System.Collections.ObjectModel;
+
+namespace StudentRegistrationApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    // Public properties bound to the UI
+    public Student StudentData { get; set; }
+    public ObservableCollection<string> ModuleOptions { get; set; }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        // Create student instance and module options
+        StudentData = new Student();
+        ModuleOptions = new ObservableCollection<string>
+            {
+                "C# Programming", "Databases", "Java", "PHP"
+            };
+
+        // Set data context for binding
+        DataContext = this;
+    }
+
+    // When submit button is clicked, generate the message
+    private void Submit_Click(object sender, RoutedEventArgs e)
+    {
+        StudentData.Submit();
+    }
+
+    private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+}
---
+++ StudentRegistrationApp/Models/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace StudentRegistrationApp.Models
+{
+    // Implements INotifyPropertyChanged to support data binding
+    public class Student : INotifyPropertyChanged
+    {
+        // Private backing fields
+        private string firstName, lastName, studentNumber, moduleName, mark, message;
+
+        // Public properties with change notifications
+        public string FirstName
+        {
+            get => firstName;
+            set { firstName = value; OnPropertyChanged(nameof(FirstName)); }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set { lastName = value; OnPropertyChanged(nameof(LastName)); }
+        }
+
+        public string StudentNumber
+        {
+            get => studentNumber;
+            set { studentNumber = value; OnPropertyChanged(nameof(StudentNumber)); }
+        }
+
+        public string ModuleName
+        {
+            get => moduleName;
+            set { moduleName = value; OnPropertyChanged(nameof(ModuleName)); }
+        }
+
+        public string Mark
+        {
+            get => mark;
+            set { mark = value; OnPropertyChanged(nameof(Mark)); }
+        }
+
+        public string Message
+        {
+            get => message;
+            set { message = value; OnPropertyChanged(nameof(Message)); }
+        }
+
+        // Method to generate a formatted result message
+        public void Submit()
+        {
+            Message = $"Student {FirstName} {LastName} ({StudentNumber}) obtained {Mark}% in {ModuleName}.";
+        }
+
+        // Required to notify the UI when a property changes
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
